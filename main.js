184bc33157a8650f63e6a523394c075ef4d42d36
Ogalo-Harry <ogaloharrison@gmail.com>
2022-05-15 19:38:55 +0300

    main.js file added

---
+++ main.js
@@ -0,0 +1,56 @@
+let quizData = [
+  {
+    question: "Which tag is used to list items with bullets?",
+    a: "<bullet>…</bullet>",
+    
+    b: "<list>…</list>",
+    
+    c: "<ul>…</ul>",
+    
+    d: "<ol>…</ol>",
+    
+    Correct: "c",
+},
+{
+  question: "How to define a link that should open in a new page in HTML?",
+  a: "<a href = “https://stackhowto.com” target = “blank”>Click Here</a>",
+  b: "<a href = “https://stackhowto.com” target =“_blank”>Click Here</a>",
+  c: "<a href = “https://stackhowto.com” target = “#blank”>Click Here</a>",
+  d: "<a href = “https://stackhowto.com” target = “@blank”>Click Here</a>",
+  
+  Correct: "d",
+},
+{
+  question: "DNS translates __________",
+a: "the domain name as IP address",
+
+b: "IP address as a domain name",
+
+c: "Both A and B are true.",
+
+d: "the domain name as physical address (MAC)",
+
+Correct: "a",
+},
+{
+  question: "How to define a background image for a web page?',
+a: "<body background = “test.jpg”>",
+b: "<body background image = “test.jpg”>",
+c: "<background = “test.jpg”>",
+d: "<background image = “test.jpg”>Answer",  
+
+Correct: "a",
+},
+{
+  question: "The first page of a website is called _____.",
+a: "Design page",
+
+b: "Home page",
+
+c: "Front page",
+
+d: "Main page",
+
+Correct: "b",
+}
+]
